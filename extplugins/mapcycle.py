169410c82c86e5711c4a890a4dd947df370b8e85
FenixXx <danielepantaleone@icloud.com>
2014-03-20 14:44:05 +0100

    automatically create database table if needed

--- extplugins/mapcycle.py
+++ extplugins/mapcycle.py
@@ -95,6 +95,18 @@ class MapcyclePlugin(b3.plugin.Plugin):
         """\
         Initialize plugin settings
         """
+        # create database table if needed
+        tables = self.console.storage.getTables()
+        if not 'maphistory' in tables:
+            self.console.storage.query("""CREATE TABLE IF NOT EXISTS `maphistory` (
+                                          `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+                                          `mapname` varchar(60) DEFAULT NULL,
+                                          `num_played` int(10) unsigned NOT NULL DEFAULT '1',
+                                          `time_add` int(10) unsigned NOT NULL,
+                                          `time_edit` int(10) unsigned NOT NULL,
+                                          PRIMARY KEY (`id`)
+                                          ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;""")
+
         # register our commands
         if 'commands' in self.config.sections():
             for cmd in self.config.options('commands'):
--- extplugins/sql/mapcycle.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `maphistory` (
-  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `mapname` varchar(60) DEFAULT NULL,
-  `num_played` int(10) unsigned NOT NULL DEFAULT '1',
-  `time_add` int(10) unsigned NOT NULL,
-  `time_edit` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
